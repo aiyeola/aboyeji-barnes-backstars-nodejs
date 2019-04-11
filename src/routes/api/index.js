2bbf8504f775deeafda4010abe3fa89241b6aae7
Ethical Ralph <rakinola90@gmail.com>
2019-04-11 19:38:47 +0100

    (routes): accommodation routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes';
+import accommodationRoutes from './accommodationRoutes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/accommodation', accommodationRoutes);
 
 export default router;
