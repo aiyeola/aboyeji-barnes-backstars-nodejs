0d36ef017757b498f820ecab4bf36983ffe6c82e
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-03-28 13:33:47 +0100

    chore(setup): express server configuration

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import '@babel/polyfill';
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import errorhandler from 'errorhandler';
+import morgan from 'morgan';
+import routes from './routes';
+
+dotenv.config();
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+app.enable('trust proxy');
+app.use(cors());
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+app.use(routes);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
