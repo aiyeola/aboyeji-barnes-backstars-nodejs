32e4c909c2934888d65320611a35a5be632c1930
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-04-01 19:03:02 +0100

    (validation): user login validation
    - modified custom error for validation schema

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,9 +1,16 @@
-/* eslint-disable require-jsdoc */
 import Joi from '@hapi/joi';
 import Schema from './schema';
 import validator from '../utils/validator';
 
+/** Validates User Input  */
 export default class userValidator {
+  /**
+   * Schema to validate signup fields
+   * @param {object} req - request object.
+   * @param {object} res - response object.
+   * @param {object} next - next middleware
+   * @returns {object} validation schema
+   */
   static async validateSignup(req, res, next) {
     const schema = Joi.object().keys({
       firstName: Schema.name,
@@ -11,8 +18,25 @@ export default class userValidator {
       userEmail: Schema.email,
       userPassword: Schema.password
     });
-    validator(schema, req.body, res);
+    validator(schema, req.body, res, next);
+  }
 
-    next();
+  /**
+   * Schema to validate login fields
+   * @param {object} req - request object.
+   * @param {object} res - response object.
+   * @param {object} next - next middleware
+   * @returns {object} validation schema
+   */
+  static async validateSignin(req, res, next) {
+    const schema = Joi.object().keys({
+      userEmail: Joi.string()
+        .required()
+        .error(new Error('email is required')),
+      userPassword: Joi.string()
+        .required()
+        .error(new Error('password is required'))
+    });
+    validator(schema, req.body, res, next);
   }
 }
