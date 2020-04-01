403d6fcbced92d7a13c3c41af19891801f6eb1dd
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-04-01 19:05:00 +0100

    (userSignUp): user sign up and sign out feature
    - user can sign up, sign in and sign out

--- src/routes/api/authRoutes.js
+++ src/routes/api/authRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import Users from '../../controllers/userController';
 import userValidation from '../../validation/userValidation';
 import method from '../../utils/method';
+import verify from '../../middlewares/auth';
 
 const router = express.Router();
 
@@ -11,4 +12,10 @@ router
   .post(userValidation.validateSignup, Users.createUser)
   .all(method);
 
+router
+  .route('/signin')
+  .post(userValidation.validateSignin, Users.login)
+  .all(method);
+
+router.route('/signout').post(verify, Users.logout);
 export default router;
