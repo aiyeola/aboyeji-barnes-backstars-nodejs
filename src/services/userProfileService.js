d874f7e11f2d038f07c50c08ad492a55740767ba
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-04-01 19:01:43 +0100

    (services): user profile services
    - modified user services

---
+++ src/services/userProfileService.js
@@ -0,0 +1,27 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable require-jsdoc */
+import database from '../database/models';
+
+const { UserProfile, Users } = database;
+
+class UserProfileService {
+ static async updateOrCreate(userId, profileData = null) {
+    try {
+      // Find a profile by userId
+      const profileFound = UserProfile.findOne({ where: { userId } });
+
+      // Create one if not found
+      if (!profileFound) await UserProfile.create({ userId });
+
+      // Update profile by userId
+      const updatedProfile = await UserProfile.update(profileData, {
+        where: { userId }
+      });
+      return updatedProfile;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default UserProfileService;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,6 @@
 /* eslint-disable*/
 import database from '../database/models';
+import UserProfile from '../services/userProfileService';
 
 const { Users } = database;
 
@@ -8,13 +9,30 @@ class UserService {
   /**
    * Creates a new user.
    * @param {object} user - user object.
-   * @returns {object} - created user object 
+   * @returns {object} - created user object
    */
   static async createUser(user) {
     try {
       const createdUser = await Users.create(user);
 
+      // Create user profile
+      await UserProfile.updateOrCreate(createdUser.id);
+      
       return createdUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Finds a user
+   * @param {string} param - param to find user
+   * @returns {object} user.
+   */
+  static async findUser(param) {
+    try {
+      const user = await Users.findOne({ where: { param } });
+      return user;
     } catch (error) {
       throw error;
     }
