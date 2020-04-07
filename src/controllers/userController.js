8cca72d0e1f5384f3b2e9c8b92c7d17b9099dfd6
Ethical Ralph <rakinola90@gmail.com>
2020-04-07 22:44:09 +0100

    initial commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,21 +6,21 @@ import userService from '../services/userService';
 import Password from '../utils/generatePassword';
 /** Class that handle user */
 class Users {
-   async createUser(req, res, next) {
-    const rawData = req.body;
+    async createUser(req, res, next) {
+        const rawData = req.body;
 
-    try {
-      // generates hashes password
-      const obj = new Password(rawData);
-      const newPassword = await obj.encryptPassword();
-      // update data
-      rawData.userPassword = newPassword;
-      const data = await userService.createUser(rawData);
-      return Response.customResponse(res, '200', 'User created', data);
-    } catch (error) {
-      return next(error);
+        try {
+            // generates hashes password
+            const obj = new Password(rawData);
+            const newPassword = await obj.encryptPassword();
+            // update data
+            rawData.userPassword = newPassword;
+            const data = await userService.createUser(rawData);
+            return Response.customResponse(res, '200', 'User created', data);
+        } catch (error) {
+            return next(error);
+        }
     }
-  }
 }
 
 export default new Users();
