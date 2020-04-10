35ea6683faac746a623df3736afea52b8f0f5f6e
Ethical Ralph <rakinola90@gmail.com>
2020-04-10 03:38:11 +0100

    Test

---
+++ src/test/server.test.js
@@ -0,0 +1,25 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import index from '../index';
+
+const server = index.app;
+
+chai.should();
+chai.use(chaiHttp);
+
+describe.skip('server test', () => {
+  it('should handle the unknown routes', done => {
+    chai
+      .request(server)
+      .get('/unknownroute')
+      .send({ data: 'nothing' })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.status.should.equal(404);
+        return done();
+      });
+  });
+});
