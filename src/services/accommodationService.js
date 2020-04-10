80f55bb6554d645f458b9657e19805801b1d3eab
Ethical Ralph <rakinola90@gmail.com>
2020-04-10 03:35:49 +0100

    (services): room and accommodation services

---
+++ src/services/accommodationService.js
@@ -0,0 +1,41 @@
+/* eslint-disable*/
+import database from '../database/models';
+
+const { Room } = database;
+
+class accommodationService {
+    /**
+     * @param {object} room - room object.
+     * @returns {object} - created room for accommodation
+     */
+    static async createRoom(room) {
+        try {
+
+            //creates room
+            const createdRoom = await Room.create(room)
+
+            return createdRoom
+        } catch (error) {
+            throw err
+        }
+    }
+
+    /**
+         * @param {id} accommodation - accommodation id.
+         * @returns {object} - rooms data
+    */
+    static async getAllRooms(id) {
+        try {
+            const data = await Room.findAll({
+                where: {
+                    accommodationId: id
+                }
+            })
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+export default accommodationService
