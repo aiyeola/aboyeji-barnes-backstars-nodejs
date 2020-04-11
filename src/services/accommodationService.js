af73505f3b3ffc7dcd8b87c36d41f712d652c420
murewaashiru <rachelleashiru@gmail.com>
2020-04-11 13:54:41 +0100

    Revert "(fix): accommodation"
    This reverts commit 68ec8a4249ecd89d50d759c93d17be4b615fc865.

--- src/services/accommodationService.js
+++ src/services/accommodationService.js
@@ -4,37 +4,38 @@ import database from '../database/models';
 const { Room } = database;
 
 class accommodationService {
-  /**
-   * @param {object} room - room object.
-   * @returns {object} - created room for accommodation
-   */
-  static async createRoom(room) {
-    try {
-      //creates room
-      const createdRoom = await Room.create(room);
+    /**
+     * @param {object} room - room object.
+     * @returns {object} - created room for accommodation
+     */
+    static async createRoom(room) {
+        try {
 
-      return createdRoom;
-    } catch (error) {
-      throw err;
-    }
-  }
+            //creates room
+            const createdRoom = await Room.create(room)
 
-  /**
-   * @param {id} accommodation - accommodation id.
-   * @returns {object} - rooms data
-   */
-  static async getAllRooms(id) {
-    try {
-      const data = await Room.findAll({
-        where: {
-          accommodationId: id
+            return createdRoom
+        } catch (error) {
+            throw err
         }
-      });
-      return data;
-    } catch (error) {
-      throw error;
     }
-  }
+
+    /**
+         * @param {id} accommodation - accommodation id.
+         * @returns {object} - rooms data
+    */
+    static async getAllRooms(id) {
+        try {
+            const data = await Room.findAll({
+                where: {
+                    accommodationId: id
+                }
+            })
+            return data
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
-export default accommodationService;
+export default accommodationService
