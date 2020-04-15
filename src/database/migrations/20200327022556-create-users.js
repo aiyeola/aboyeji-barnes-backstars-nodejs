8e872499abaffaa08774577686b72d271e85acec
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-04-15 12:42:07 +0100

    (fix): prettier code

--- src/database/migrations/20200327022556-create-users.js
+++ src/database/migrations/20200327022556-create-users.js
@@ -1,46 +1,46 @@
 /* eslint-disable */
 export default {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable('Users', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            firstName: {
-                type: Sequelize.STRING
-            },
-            lastName: {
-                type: Sequelize.STRING
-            },
-            userEmail: {
-                type: Sequelize.STRING
-            },
-            userPassword: {
-                type: Sequelize.STRING
-            },
-            userRoles: {
-                type: Sequelize.STRING
-            },
-            accountVerified: {
-                type: Sequelize.BOOLEAN,
-                defaultValue: false
-            },
-            emailAllowed: {
-                type: Sequelize.BOOLEAN
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
-    },
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('Users');
-    }
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      userEmail: {
+        type: Sequelize.STRING
+      },
+      userPassword: {
+        type: Sequelize.STRING
+      },
+      userRoles: {
+        type: Sequelize.STRING
+      },
+      accountVerified: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      emailAllowed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
 };
