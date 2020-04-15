312b95745dba078d503b6a52afa43977b4f616e8
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-04-15 12:10:47 +0100

    (test): server and user authRoutes test

---
+++ src/test/user.test.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-else-return */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import passport from 'passport';
+import index from '../index';
+import '../config/passport';
+
+const server = index.app;
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+const signUpURL = '/api/v1/auth/signup';
+const signInURL = '/api/v1/auth/signin';
+const signOut = '/api/v1/auth/signout';
+const loginWithFacebook = '/api/v1/auth/facebook';
+const facebookRedirect = '/api/v1/auth/facebook/redirect';
+const loginWithGoogle = '/api/v1/auth/google';
+const googleRedirect = '/api/v1/auth/google/redirect';
+
+let token;
+
+const regData = {
+  userEmail: 'jonathanaurugai@gmail.com',
+  firstName: 'Jonathan',
+  lastName: 'Aurugai',
+  userPassword: 'Root1234@',
+};
+const regDataWithWrongEmail = {
+  userEmail: 'jonathanaurugaigmail.com',
+  firstName: 'Jonathan',
+  lastName: 'Aurugai',
+  userPassword: 'Root',
+};
+
+describe.skip('Create an account', () => {
+  it('and send an email with verification link');
+  it('with valid properties ', (done) => {
+    chai
+      .request(server)
+      .post(signUpURL)
+      .send(regData)
+      .end((_err, res) => {
+        // token1 = res.body.data.userToken;
+        expect(res.body.message).to.equal(
+          'Account has been created successfully'
+        );
+        expect(res.status).to.equal(201);
+        done();
+      });
+  });
+  it('with wrong Email', (done) => {
+    chai
+      .request(server)
+      .post(signUpURL)
+      .send(regDataWithWrongEmail)
+      .end((_err, res) => {
+        expect(res.status).to.eq(422);
+        done();
+      });
+  });
+});
+
+describe('User Login', () => {
+  it('with correct credentials', (done) => {
+    const user = {
+      userEmail: 'jonathanaurugai@gmail.com',
+      userPassword: 'Root1234@',
+    };
+    chai
+      .request(server)
+      .post(signInURL)
+      .send(user)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        } else {
+          expect(res).to.have.status(200);
+          token = res.body.data;
+          console.log(token);
+          done();
+        }
+      });
+  });
+});
+
+describe.skip('Users Logout', () => {
+  it('when they are logged In', (done) => {
+    chai
+      .request(server)
+      .post(signOut)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+      .end((_err, res) => {
+        if (_err) done(_err);
+        console.log(res.body);
+        expect(res.body.message).to.eq('User logged out successfully');
+        expect(res.status).to.eq(200);
+        done();
+      });
+  });
+  // it('when they are not logged In', (done) => {
+  //   chai
+  //     .request(server)
+  //     .post(signOut)
+  //     .set('Authorization', `Bearer ${token}`)
+  //     .send()
+  //     .end((_err, res) => {
+  //       if (_err) done(_err);
+
+  //       expect(res.body.message).to.eq('User not logged In');
+  //       expect(res.status).to.eq(401);
+  //       done();
+  //     });
+  // });
+
+  // it('by passing incorrect token', (done) => {
+  //   chai
+  //     .request(server)
+  //     .post(signOut)
+  //     .set('Authorization', 'WrongToken sadasdasdasd')
+  //     .send()
+  //     .end((_err, res) => {
+  //       if (_err) done(_err);
+
+  //       expect(res.body.message).to.eq('Invalid or expired token used');
+  //       expect(res.status).to.eq(401);
+  //       done();
+  //     });
+  // });
+});
