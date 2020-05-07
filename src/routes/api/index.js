430023bc5c97a242515c914c30b6904092c83b56
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-05-07 02:23:31 +0100

    refactor

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,22 +1,17 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes';
-
-import bookingRoute from "./bookingRoute"
-const router = Router();
-
-router.use('/auth', authRoutes);
-router.use("/booking",bookingRoute)
-
 import accommodationRoutes from './accommodationRoutes';
 import locationRoutes from './locationRoutes';
 import profileRoutes from './userProfileRoutes';
+import bookingRoute from './bookingRoute';
 
 const router = Router();
 
+router.use('/auth', authRoutes);
+router.use('/booking', bookingRoute);
 router.use('/auth', authRoutes);
 router.use('/accommodation', accommodationRoutes);
 router.use('/locations', locationRoutes);
 router.use('/profile', profileRoutes);
-
 
 export default router;
